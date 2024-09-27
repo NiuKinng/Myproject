d20faaad8aca1cb3a5dd39ae4a55e138cd7d31a3
NiuKinng <2825904183@qq.com>
2024-09-27 11:58:28 +0800

    Add files via upload

---
+++ QQServer/src/MyQQ/Conmmon/Massage.java
@@ -0,0 +1,101 @@
+package MyQQ.Conmmon;
+
+import java.io.Serializable;
+
+public class Massage implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private String Sender;
+    private String Receiver;
+    private String Content;
+    private String Sendtime;
+    private String MassageType;
+    private byte[] fileBytes;
+    private int flieLen;
+    private String dest;
+    private String src;
+
+
+    public String getMassageType() {
+        return MassageType;
+    }
+
+    public void setMassageType(String massageType) {
+        MassageType = massageType;
+    }
+    public Massage(){
+
+    }
+
+    public Massage(String sender, String receiver, String content, String sendtime) {
+        Sender = sender;
+        Receiver = receiver;
+        Content = content;
+        Sendtime = sendtime;
+    }
+
+    public String getReceiver() {
+        return Receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        Receiver = receiver;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
+
+    public String getSendtime() {
+        return Sendtime;
+    }
+
+    public void setSendtime(String sendtime) {
+        Sendtime = sendtime;
+    }
+
+    public String getSender() {
+        return Sender;
+    }
+
+    public void setSender(String sender) {
+        Sender = sender;
+    }
+
+    public byte[] getFileBytes() {
+        return fileBytes;
+    }
+
+    public void setFileBytes(byte[] fileBytes) {
+        this.fileBytes = fileBytes;
+    }
+
+    public int getFlieLen() {
+        return flieLen;
+    }
+
+    public void setFlieLen(int flieLen) {
+        this.flieLen = flieLen;
+    }
+
+    public String getDest() {
+        return dest;
+    }
+
+    public void setDest(String dest) {
+        this.dest = dest;
+    }
+
+    public String getSrc() {
+        return src;
+    }
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+}
---
+++ QQServer/src/MyQQ/qqserver/ServerConnectClientThread.java
@@ -0,0 +1,98 @@
+package MyQQ.qqserver;
+
+import MyQQ.Conmmon.Massage;
+import MyQQ.Conmmon.MassageType;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Set;
+
+public class ServerConnectClientThread extends Thread {
+    private Socket socket;
+    private String username;
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public ServerConnectClientThread(Socket socket, String username) {
+        this.socket = socket;
+        this.username = username;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                System.out.println("服务端和客户端" + username + "保持通信，读取数据...");
+                ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+                ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                Massage massage = (Massage) ois.readObject();
+                if (massage.getMassageType().equals(MassageType.MASSAGE_GET_OLINE_FRIED)) {
+                    System.out.println(massage.getSender() + "要在线用户列表");
+                    String Olineusers = ManageClientThreads.getOlineusers();
+                    Massage massage1 = new Massage();
+                    massage1.setMassageType(MassageType.MASSAGE_RET_OLINE_FRIED);
+                    massage1.setContent(Olineusers);
+                    massage1.setReceiver(massage.getSender());
+                    oos.writeObject(massage1);
+                } else if (massage.getMassageType().equals(MassageType.MASSAGE_CLIENT_EXIT)) {
+                    System.out.println(massage.getSender() + "退出系统。");
+                    ManageClientThreads.removeClientThreads(massage.getSender());
+                    socket.close();
+                    break;
+                } else if (massage.getMassageType().equals(MassageType.MASSAGE_COMM_MES)) {
+                    HashMap<String, ServerConnectClientThread> hm = ManageClientThreads.getHm();
+                    Set<String> set = hm.keySet();
+                    if (set.contains(massage.getReceiver())) {
+                        try {
+                            ObjectOutputStream oos1 = new ObjectOutputStream(ManageClientThreads.getClientThread(massage.getReceiver()).socket.getOutputStream());
+                            oos1.writeObject(massage);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    } else {
+                        HashMap<String, LinkedList<String>> newhm = ManageClientThreads.getNewhm();
+                        if (newhm.containsKey(massage.getReceiver())) {
+                            newhm.get(massage.getReceiver()).add(massage.getSender()+"对你说："+massage.getContent());
+                        } else {
+                            LinkedList<String> list = new LinkedList<>();
+                            list.add(massage.getSender()+"对你说："+massage.getContent());
+                            newhm.put(massage.getReceiver(), list);
+                        }
+//                        LinkedList<String> strings = new LinkedList<>();
+//                        strings.add("我想吃鱼了");
+//                        newhm.put("200",strings);
+                        ManageClientThreads.setNewhm(newhm);
+                    }
+
+                } else if (massage.getMassageType().equals(MassageType.MASSAGE_TO_ALL)) {
+                    HashMap<String, ServerConnectClientThread> hm = ManageClientThreads.getHm();
+                    Set<String> usernames = hm.keySet();
+                    for (String s : usernames) {
+                        if (s != username) {
+                            ObjectOutputStream oos1 = new ObjectOutputStream(ManageClientThreads.getClientThread(s).socket.getOutputStream());
+                            oos1.writeObject(massage);
+                        }
+
+                    }
+
+
+                } else if (massage.getMassageType().equals(MassageType.MASSAGE_FEIL_MES)) {
+                    ObjectOutputStream oos1 = new ObjectOutputStream(ManageClientThreads.getClientThread(massage.getReceiver()).socket.getOutputStream());
+                    oos1.writeObject(massage);
+
+
+                }
+
+            } catch (IOException | ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
